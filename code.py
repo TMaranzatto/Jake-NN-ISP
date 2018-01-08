65c1f2ad25b8fbde51465e0ec6ac84bdf271024c
Jake Maranzatto <maranzatto1@yahoo.com>
2018-01-07 21:13:04 -0500

    Input and Hidden Nodes
    Some code for input and hidden nodes, as well as some preliminary code for the input layer.  we need:
    1. hidden layer structure
    2. whole network structure
    3. forward propogation code
    4. back propogation code

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import random
+import math
 class simplePerceptron:
     #trying to get the hang of the interactions between neurons...
     #perceptrons may help with this
@@ -6,7 +7,6 @@ class simplePerceptron:
     def __init__(self, n):
         self.weights = [random.uniform(-1,1) for _ in range(n)]
         self.constant = 0.01
-        print(self.weights)
         
     def activate(self, number):
         if number > 0:
@@ -28,12 +28,54 @@ class simplePerceptron:
             self.weights[i] += constant * error * inputs[i]
 
 class Trainer:
-    x = [] #placeholder
+    def __init__(self, x, y, a):
+        self.inputs = [x, y, 1]
+        self.answer = a
 
-p = simplePerceptron(3)
-point = [10, -2, 1]
-result = p.feedforward(point)
-print(result)
-#class sumOps:
+##################################################################
+
+        
+class sumOpsActive:
+    def __init__(self, a, c):
+        #a is the number of weights needed.
+        #it is the same as the number of neurons in the previous layer
+        #c is the neurons bias
+        self.weights = [random.uniform(-1,1) for _ in range(a)]
+        self.constant = c
+
+    def g(x):
+        return 1/(1+math.pow(math.e, x * -1))
+        
+    def output(self, v):
+        #takes in a vector of inputs v, multiplies each element by the set weight, and adds bias
+        #then pumps this through g(x) which is standard logistic function
+        u = self.constant + sum([sum(x) for x in zip(self.weights, v)])
+        return g(u)
     
-#class activationOps:
+class inputOps:
+    def __init__(self, inn):
+        #inn is a single value
+        self.value = inn
+
+    def output(self)
+        #as this is an input node, we only need to return value
+        return self.value
+    
+# do we need a class outputOps:? I dont think so
+class inputLayer:
+    def __init__(self, n):
+        #n is the number of input nodes
+        #we initialize everything with a value of 0
+        self.inputs = [inputOps(0) for x in range(n)]
+        
+        
+class hiddenLayer:
+
+class network:
+
+
+
+
+
+
+
